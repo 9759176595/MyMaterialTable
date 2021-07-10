f429dc8f7a8f5ff2a6493c8f1bfb0dec6316d6bc
Raj Kamal Shakya <rajkamalshakya2000@gmail.com>
2021-07-10 19:45:49 +0530

    learn coding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react'
 import './App.css';
+import MaterialTable from 'material-table'
 
-function App() {
-  return (
+
+ function App() {
+   const url= "https://jsonplaceholder.typicode.com/users"
+     const[data, setData]=useState();
+
+     useEffect(()=>{
+      getEmployee()
+  },[])
+  const getEmployee=()=>{
+    fetch(url)
+      .then(resp=>resp.json())
+      .then(resp=>{
+        setData(resp)})
+  }
+
+     const columns=[
+         {title: "Id",field: 'id'},
+         {title: "Name",field: "name",validate:rowData=>rowData.name===undefined || rowData.name===""?"Required":true},
+         {title: "Username",field: "username"},
+         {title: "Email",field: "email"},
+         {title: "Phone",field: "phone"},
+         {title: "Website",field: "website"}
+     ]
+    
+     
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 align="center">Nemesis Consultants LLP 💟  </h1>
+        {/* <h4 align="center"></h4> */}
+       <MaterialTable 
+       title="Nemesis Employee Data"
+       data={data}
+       columns={columns}
+       options={{
+        actionColumnIndex: -1, addRowPosition:"first",
+           search: false,
+           paging:false,
+          //  filtering: true,
+           exportButton: true,
+       }}
+
+       editable={{
+          onRowAdd: (newRow)=>new Promise((resolve, reject)=>{
+            const updateRows=[...data,newRow]
+            setTimeout(()=>{
+              setData(updateRows)
+              resolve()
+            },1000)
+          }),
+          onRowDelete: selectedRow=> new Promise((resolve, reject)=>{
+            const index=selectedRow.tableData.id;
+            const updateRows=[...data]
+            updateRows.splice(index,1)
+            setTimeout(()=>{
+              setData(updateRows)
+              resolve()
+            },1000)
+          }),
+          onRowUpdate: (updatedRow,oldRow)=>new Promise((resolve, reject)=>{
+           const index=oldRow.tableData.id;
+           const updatedRows=[...data]
+           updatedRows[index]=updatedRow
+            setTimeout(()=>{
+              setData(updatedRows)
+              resolve()
+            },1000)
+          })
+       }}
+     
+       />
     </div>
-  );
+    )
 }
 
 export default App;
